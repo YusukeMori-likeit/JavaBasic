2450df6a09f708603334cdc1a1a3ee12f17b02b0
Yusuke Mori <kotori382@gmail.com>
2018-05-09 15:51:03 +0900

    PTra05_04 まで完了

--- Practice/src/practice05/PTra05_04.java
+++ Practice/src/practice05/PTra05_04.java
@@ -23,16 +23,46 @@ public class PTra05_04 {
 		 */
 
 		// ①
-		System.out.println("0～6の整数を入力してください");
+		boolean winFlg = true;
 
-		String line = scanner.nextLine();
+		while (winFlg) {
 
-		switch(line) {
-			case "0":
+			System.out.println("0～6の整数を入力してください");
+
+			String line = scanner.nextLine();
+
+			int date = Integer.parseInt(line);
+
+			switch (date) {
+			case 0:
 				System.out.println("日曜");
 				break;
-			case "1":
+			case 1:
 				System.out.println("月曜");
+				break;
+			case 2:
+				System.out.println("火曜");
+				break;
+			case 3:
+				System.out.println("水曜");
+				break;
+			case 4:
+				System.out.println("木曜");
+				break;
+			case 5:
+				System.out.println("金曜");
+				break;
+			case 6:
+				System.out.println("土曜");
+				break;
+			default:
+				
+				continue;
+
+			}
+			winFlg = false;
+
+
 		}
 	}
 }
