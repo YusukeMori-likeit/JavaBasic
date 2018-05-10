e819c67019cf4249257f8675da195d1793b9334a
Yusuke Mori <kotori382@gmail.com>
2018-05-10 17:55:59 +0900

    PTra10_01 まで完了

--- Practice/src/practice10/PTra10_01.java
+++ Practice/src/practice10/PTra10_01.java
@@ -27,10 +27,16 @@ public class PTra10_01 {
 		// Userクラスを作成後に着手してください
 		// ★ User型の変数usを宣言してください
 
+		User us = new User() ;
+
 		// ★ 変数usに、Userクラスのインスタンスを作成・代入してください
 
+		us.userId = 1;
+		us.userNm = "yusuke";
+		us.mail = "a@aaa";
+		us.password = "1234";
 
 		// ★ 変数usに格納されているインスタンスの、フィールドuserId, userNm, mail, passwordを出力してください
-
+		System.out.println(us.userId+ ","+ us.userNm+","+us.mail+"," + us.password);
 	}
 }
