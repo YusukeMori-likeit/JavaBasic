bbbc605d5282500b463000076f9987dc81a2cde1
Yusuke Mori <kotori382@gmail.com>
2018-05-10 10:34:36 +0900

    PTra06_07 まで完了

--- Practice/src/practice06/PTra06_07.java
+++ Practice/src/practice06/PTra06_07.java
@@ -25,5 +25,38 @@ public class PTra06_07 {
 		 * ■□□□□
 		 */
 
+		for(int i = 1;i <= 5;i++) {
+			for(int j = 1;j <= 5;j++) {
+				if(j > (i - 1)) {
+					System.out.print("□");
+				}else {
+					System.out.print("■");
+				}
+			}
+			System.out.println();
+
+		}
+		for(int k = 1;k <= 5;k++) {
+			
+			for(int l = 1;l <= 5;l++) {
+				
+				if(((k+l) >= 7 && (k+l) <= 10)) {
+					
+					System.out.print("□");
+					
+				}else {
+					System.out.print("■");
+				}
+				
+			}
+			System.out.println();
+		}
+		
+		
+		}
+		
+		
+
 	}
-}
+	
+
