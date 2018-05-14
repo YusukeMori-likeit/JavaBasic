f3952a6ce0d1c772e1f27c3ddbca33d3b735acf0
Yusuke Mori <kotori382@gmail.com>
2018-05-14 17:28:20 +0900

    PTra10_06 まで完了

--- Practice/src/practice10/PTra10_06.java
+++ Practice/src/practice10/PTra10_06.java
@@ -18,14 +18,14 @@ public class PTra10_06 {
 		 * carインスタンスを3件作成し、それぞれの色、ガソリンを入力して決定してください
 		 * 各carインスタンスのrunメソッドを実行して、それぞれ「目的地にまでn時間かかりました。残りのガソリンは、xリットルです」を出力してください。
 		 */
-		
+
 		Car[] cars = new Car[3];
 		cars[0] = new Car();
 		cars[1] = new Car();
 		cars[2] = new Car();
-		
-		
-		
+
+
+
 		cars[0].serialNo = 0;
 		cars[0].color = "Red";
 		cars[0].gasoline = 50;
@@ -35,72 +35,36 @@ public class PTra10_06 {
 		cars[2].serialNo = 2;
 		cars[2].color ="blue";
 		cars[2].gasoline = 200;
-		
-		int sum = 0;
-		int count = 0;
+
 		final int distance = 300;
-		
-		
-		
-		while(true) {
-			
-		
-			int run = cars[0].run();
-			count++;
 
-			if(run == -1) {
-				System.out.println("目的地に到達できませんでした。");
-				break;
+		for(int i = 0;i < cars.length;i++) {
+			int sum0 = 0;
+			int count0 = 0;
+
+			while(true) {
+
+
+				int run = cars[i].run();
+				count0++;
+
+				if(run == -1) {
+					System.out.println("目的地に到達できませんでした。");
+					break;
+				}
+
+				sum0 += run;
+//				System.out.println(sum0);
+
+				if(distance < sum0 ) {
+					System.out.println(cars[i].color + "は目的地にまで" + count0 + "時間かかりました。残りのガソリンは、"+cars[i].gasoline);
+					break;
 			}
 
-			sum += run;
-			System.out.println(sum);
-
-			if(distance < sum ) {
-				System.out.println("car0は目的地にまで" + count + "時間かかりました。残りのガソリンは、"+cars[0].gasoline);
-				break;
-		}
-		
-		}
-		int a = 0;
-		int c = 0;
-		while(true) {
-			int run = cars[1].run();
-			a++;
-
-			if(run == -1) {
-				System.out.println("目的地に到達できませんでした。");
-				break;
 			}
-
-			c += run;
-			System.out.println(c);
-
-			if(distance < c ) {
-				System.out.println("car1は目的地にまで" + a + "時間かかりました。残りのガソリンは、"+cars[1].gasoline);
-				break;
 		}
-		}
-		int b = 0;
-		int d = 0;
-		
-		while(true) {
-			int run = cars[2].run();
-			b++;
 
-			if(run == -1) {
-				System.out.println("目的地に到達できませんでした。");
-				break;
-			}
-
-			d += run;
-			System.out.println(d);
-
-			if(distance < d ) {
-				System.out.println("car2は目的地にまで" + b + "時間かかりました。残りのガソリンは、"+cars[2].gasoline);
-				break;
-		}
-		}
 
 	}
 }
+
