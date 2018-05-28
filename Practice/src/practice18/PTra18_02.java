0c48232727e6459032ca4c6bcccd1531b41a4335
Yusuke Mori <kotori382@gmail.com>
2018-05-28 15:58:10 +0900

    PTra18_04 まで完了

--- Practice/src/practice18/PTra18_02.java
+++ Practice/src/practice18/PTra18_02.java
@@ -6,6 +6,13 @@
  */
 package practice18;
 
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.util.ArrayList;
+import java.util.Scanner;
+
+import practice18.entity.Player;
+
 public class PTra18_02 {
 
 	/*
@@ -31,10 +38,34 @@ public class PTra18_02 {
 		 * ★ file/BestElevenCandidate.csvの内容を取得し、１行毎にPlayerインスタンスに情報を格納してください
 		 * ★ ArrayListを作成して、Playerインスタンスを格納してください
 		 */
+        ArrayList<Player> array = new ArrayList<>();
+        try(Scanner scanner = new Scanner(new File("file/BestElevenCandidate.csv"))) {
+            while (scanner.hasNext()) {
+                String line = scanner.nextLine();
+                String[] word = line.split(",",0);
+
+                Player player = new Player();
+                player.setPosition(word[0]);
+                player.setName(word[1]);
+                player.setCountry(word[2]);
+                player.setTeam(word[3]);
+
+                array.add(player);
+            }
+        } catch (FileNotFoundException e) {
+			e.printStackTrace();
+		}
+
+
+
 
 
 		// ★ ArrayListに格納されているインスタンス全てのtoStringメソッドを実行し、出力してください
 		// ※ できれば拡張for文を使いましょう
+        for(Player players : array) {
+        	System.out.println(players);
+        }
+        
 
 	}
 }
--- Practice/src/practice18/PTra18_04.java
+++ Practice/src/practice18/PTra18_04.java
@@ -6,6 +6,14 @@
  */
 package practice18;
 
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.Scanner;
+
+import practice18.entity.Player;
+
 public class PTra18_04 {
 
 	/*
@@ -19,9 +27,69 @@ public class PTra18_04 {
 		 * ★ file/BestElevenCandidate.csvの内容を取得し、１行毎にPlayerインスタンスに情報を格納してください
 		 * ★ ArrayListを作成して、Playerインスタンスを格納してください
 		 */
+        ArrayList<Player> array = new ArrayList<>();
+        try(Scanner scanner = new Scanner(new File("file/BestElevenCandidate.csv"))) {
+            while (scanner.hasNext()) {
+                String line = scanner.nextLine();
+                String[] word = line.split(",",0);
+
+                Player player = new Player();
+                player.setPosition(word[0]);
+                player.setName(word[1]);
+                player.setCountry(word[2]);
+                player.setTeam(word[3]);
+
+                array.add(player);
+            }
+        } catch (FileNotFoundException e) {
+			e.printStackTrace();
+		}
 
 
 		// ★ ①のArrayListの中からGK1名、DF4名、MF4名, FW2名をランダムで出力してください
+        int a = 0;
+        int b = 0;
+        int c = 0;
+        int d = 0;
+        
+        Collections.shuffle(array);
+        for(int i = 0;i < array.size();i++) {
+        	if(array.get(i).getPosition().equals("GK")) {
+        		System.out.println(array.get(i).toString());
+        	
+        		a++;
+        	}
+        	if(a == 1) {
+        		break;
+        	}
+        }
+        for(int i = 0;i < array.size();i++) {
+        	if(array.get(i).getPosition().equals("DF")) {
+        		System.out.println(array.get(i).toString());
+        		b++;
+        	}
+        	if(b == 4) {
+        		break;
+        	}
+        }
+        for(int i = 0;i < array.size();i++) {
+        	if(array.get(i).getPosition().equals("MF")){
+        		System.out.println(array.get(i).toString());
+        	c++;
+        	}
+        	if(c == 4) {
+        		break;
+        	}
+        }
+        for(int i = 0;i<array.size();i++) {
+        	if(array.get(i).getPosition().equals("FW")) {
+        		System.out.println(array.get(i).toString());
+        		d++;
+        	}
+        	if(d == 2) {
+        		break;
+        	}
+        }
 
 	}
 }
